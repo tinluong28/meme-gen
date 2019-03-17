74e47af3c26d1b261aa5ea3ab9d6304baf4b92f0
tinluong28 <tinluong28@gmail.com>
2019-03-17 00:19:52 -0700

    add creat meme form

--- src/component/App.js
+++ src/component/App.js
@@ -2,21 +2,49 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import MemeItem from "./MemeItem";
 import "../index.css";
+
+import { Form, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      memeLimit: 10
+      memeLimit: 10,
+      text0: "",
+      text1: ""
     };
   }
   render() {
     return (
       <div>
-        <h2>Welcome to the Meme Generator!</h2>
+        <h2>
+          <u>Welcome to the Meme Generator!</u>
+        </h2>
+        <h4>
+          <i>Write Some Text</i>
+        </h4>
+        <Form inline>
+          <FormGroup>
+            <FormLabel>Top</FormLabel>
+            <FormControl
+              type="text"
+              onChange={e => this.setState({ text0: e.target.value })}
+              placeholder="Type something..."
+            />
+          </FormGroup>{" "}
+          <FormGroup>
+            <FormLabel>Bottom</FormLabel>
+            <FormControl
+              type="text"
+              onChange={e => this.setState({ text1: e.target.value })}
+              placeholder="Type something..."
+            />
+          </FormGroup>
+        </Form>
         {this.props.meme.slice(0, this.state.memeLimit).map(meme => (
           <MemeItem key={meme.id} meme={meme} />
         ))}
         <p
+          className="LoadMore"
           onClick={() => {
             this.setState({ memeLimit: this.state.memeLimit + 10 });
           }}
