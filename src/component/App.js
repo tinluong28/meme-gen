b3d5c277f325fdde344a3b6f0c700082d28909eb
tinluong28 <tinluong28@gmail.com>
2019-03-17 00:41:06 -0700

    rearrange form

--- src/component/App.js
+++ src/component/App.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import MemeItem from "./MemeItem";
 import "../index.css";
 
-import { Form, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Form, FormGroup, FormControl, FormLabel, Col } from "react-bootstrap";
 class App extends Component {
   constructor() {
     super();
@@ -16,29 +16,35 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h2>
+        <h2 className="mb-5">
           <u>Welcome to the Meme Generator!</u>
         </h2>
-        <h4>
-          <i>Write Some Text</i>
-        </h4>
+
         <Form inline>
-          <FormGroup>
-            <FormLabel>Top</FormLabel>
-            <FormControl
-              type="text"
-              onChange={e => this.setState({ text0: e.target.value })}
-              placeholder="Type something..."
-            />
-          </FormGroup>{" "}
-          <FormGroup>
-            <FormLabel>Bottom</FormLabel>
-            <FormControl
-              type="text"
-              onChange={e => this.setState({ text1: e.target.value })}
-              placeholder="Type something..."
-            />
-          </FormGroup>
+          <Col md="auto">
+            <FormGroup>
+              <FormLabel className="pr-3">
+                <strong>Top</strong>
+              </FormLabel>
+              <FormControl
+                type="text"
+                onChange={e => this.setState({ text0: e.target.value })}
+                placeholder="Write some text..."
+              />
+            </FormGroup>
+          </Col>{" "}
+          <Col md="auto">
+            <FormGroup>
+              <FormLabel className="pr-3">
+                <strong>Bottom</strong>
+              </FormLabel>
+              <FormControl
+                type="text"
+                onChange={e => this.setState({ text1: e.target.value })}
+                placeholder="Write some text..."
+              />
+            </FormGroup>
+          </Col>
         </Form>
         {this.props.meme.slice(0, this.state.memeLimit).map(meme => (
           <MemeItem key={meme.id} meme={meme} />
